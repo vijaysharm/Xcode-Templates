0eea0ab3329d2805babea1f5609be1801726ed28
vijaysharm <vijays@scribd.com>
2026-01-17 08:02:24 -0500

    Added Default and Internal actions as default

--- File Templates/TCA Reducer with View.xctemplate/___FILEBASENAME___.swift
+++ File Templates/TCA Reducer with View.xctemplate/___FILEBASENAME___.swift
@@ -4,11 +4,20 @@ import ComposableArchitecture
 import SwiftUI
 
 @Reducer
-struct ___FILEBASENAMEASIDENTIFIER___Feature {
+struct ___FILEBASENAMEASIDENTIFIER___Feature: Sendable {
     @ObservableState
-    struct State: Equatable {}
+    struct State: Equatable, Sendable {}
     
-    enum Action: Equatable {}
+    enum Action: Equatable, Sendable {
+        @CasePathable
+        enum DelegateAction: Equatable, Sendable {}
+
+        @CasePathable
+        enum InternalAction: Equatable, Sendable {}
+
+        case `internal`(InternalAction)
+        case delegate(DelegateAction)
+    }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
